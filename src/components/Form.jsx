2c00c2afb70459d0c1336cccc7983a637d3e25b7
Xolelani Mdima <xolelanimdima@gmail.com>
2022-12-09 12:50:16 +0200

    add files

---
+++ src/components/Form.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+
+function Form() {
+  return (
+  <>
+  <Sidebar />
+
+  <div className='form-info'>
+      
+  </div>
+  
+  </>
+  )
+}
+
+export default Form
